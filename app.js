563b36a67c9528583d21959f52b5da2445be7d8b
Sanskar2001 <sanskaratrey22@gmail.com>
2021-12-04 14:59:19 +0530

    added database

--- app.js
+++ app.js
@@ -4,7 +4,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash=require('lodash/string')
+const mongoose=require('mongoose')
 var items=[];
+mongoose.connect('mongodb://localhost:27017/blogDb');
+
+const Schema=new mongoose.Schema({
+    title:String,
+    content:String});
+
+const Blogs=mongoose.model("Blogs",Schema);
+
+Blogs.find(function (err,blog) {
+    if(err)
+    console.log(err);
+    blog.forEach(element => {
+        const currBlog={
+            title:element.title,
+            post:element.content
+           }
+           items.push(currBlog)
+    })
+});
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -67,11 +87,23 @@ app.post('/',(req,res)=>{
     var composedBlog={
         title:req.body.titleFormInput,
         post:req.body.postFormInput
+       
+
     }
+    const blog= new Blogs({
+        title:composedBlog.title,
+        content:composedBlog.post
+
+    })
+    blog.save(()=>{
+        console.log;("blog saved");
+        res.redirect('/')
+    });
+    console.log(blog.title);
     // console.log(composedBlog)
     items.push(composedBlog)
     // console.log(items)
-    res.redirect('/')
+   
 
 })
 
